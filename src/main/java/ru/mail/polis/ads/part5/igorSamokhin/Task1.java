b21d60dda0cc228d951bf610750e41cc106c4383
IgorSamohin <igor04012001@yandex.ru>
2020-11-02 15:01:40 +0300

    Complete task1

--- src/main/java/ru/mail/polis/ads/part5/igorSamokhin/Task1.java
+++ src/main/java/ru/mail/polis/ads/part5/igorSamokhin/Task1.java
@@ -5,60 +5,66 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
+//https://www.e-olymp.com/ru/submissions/7619084
 public final class Task1 {
     private static void solve(final FastScanner in, final PrintWriter out) {
         int N = in.nextInt();
 
-        StringBuilder arr = new StringBuilder();
+        StringBuilder arrq = new StringBuilder();
         ArrayList<Integer> arr = new ArrayList<Integer>(N);
 
         for (int i = 1; i <= N; i++) {
-            arr.set(i - 1, i);
+            arr.add(i - 1, i);
         }
 
         while (true) {
-
             if (N == 1) {
                 System.out.println(arr.get(0));
                 break;
             }
+            for (int i = 0; i < N; i++) {
+                out.print(arr.get(i));
+                out.print(" ");
+            }
+            out.println("");
 
-            int min = arr.get(N - 1);
             int index = N - 1;
-            int minIndex = arr.get(N - 1);
-            for (int i = N - 2; i > 0; i--) {
-                if (arr.get(i) < min) {
-                    min = arr.get(i);
-                    minIndex = i;
-                }
-
-                if (arr.get(i + 1) > arr.get(i)) {
-                    index = i;
+            for (int i = N - 2; (i >= 0 )&& (index != 0); --i) {
+                if (arr.get(i) > arr.get(index)) {
+                    --index;
+                } else {
                     break;
                 }
             }
 
-            int temp = arr.get(index);
-            arr.set(index, min);
+            if(index == 0){
+                break;
+            }
+
+            int min = arr.get(N - 1);
+            int minIndex = N - 1;
+            for (int i = N - 1; i >= index; i--) {
+                if (arr.get(i) > arr.get(index - 1)) {
+                    min = arr.get(i);
+                    minIndex = i;
+                    break;
+                }
+            }
+
+            int temp = arr.get(index - 1);
+            arr.set(index - 1, min);
             arr.set(minIndex, temp);
 
-            for(int i = index; i < N; i++)
-            {
-                Collections.swap(arr, i, N-i);
-                //do reverse from index to end
+            int k = N;
+            for (int i = index; i < k; i++, k--) {
+                Collections.swap(arr, i, k - 1);
             }
 
-
-
-            for (int i = 0; i < arr.length(); i++) {
-                out.print(arr.charAt(i));
-                out.print(" ");
-            }
+            --index;
         }
 
     }
